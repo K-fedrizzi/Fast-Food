6f39bbd0bdcd0fb27a08467d44a40d2bad0ce9f6
Tiago Menezes <39500750+TiagoMenezesVarmassera@users.noreply.github.com>
2020-10-10 08:06:27 -0400

    Add files via upload

--- site/listaJS.js
+++ site/listaJS.js
@@ -1,16 +1,25 @@
 var bt=document.getElementById("expandirBt");
-
-
+var avaliBt=document.getElementById("avaliacoesBt");
 function expandir(){
  if(bt.textContent=="Ler Mais"){
     var caixaText=document.getElementById("descricaoTx");  
-   caixaText.style.height='auto';
-   
-bt.textContent="Ler Menos";}
+bt.textContent="Ler Menos";
+caixaText.style.height="auto";
+}
 else{
-
     var caixaText=document.getElementById("descricaoTx");  
-    caixaText.style.height='40px';
-bt.textContent="Ler Mais";}
+    caixaText.style.height="40px";
+bt.textContent="Ler Mais"};
+}
+
+function avaliacaoBt(){
+ 
+    var caixaText=document.getElementById("avaliacaoTx");  
+caixaText.style.display="inline";
+}
+function minimizarAvaliacao(){
+ 
+  var caixaText=document.getElementById("avaliacaoTx");  
+caixaText.style.display="none";
 }
 
