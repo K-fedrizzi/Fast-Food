7ea140100f9a1f8c20e50d594d83551b6e156f8f
Tiago Menezes <39500750+TiagoMenezesVarmassera@users.noreply.github.com>
2020-11-24 02:55:49 -0400

    Add files via upload

--- visualizacao/lista.php
+++ visualizacao/lista.php
@@ -11,7 +11,29 @@
     </head>
 
     <body>
-            
+    <?php
+
+include_once '.\..\modelo\Banco.php';
+include_once '.\..\modelo\Cliente.php';
+include_once '.\..\modelo\Empresa.php';
+include_once '.\..\modelo\Produto.php';
+include_once '.\..\modelo\Cliente_avaliacao_produto.php';
+/**
+ * Cria um banco de dados para o projeto
+ */
+try{
+
+    Banco::createSchema();
+    
+} catch (\Throwable $th) {
+    echo $th;
+    die(1);
+}
+
+$produtos=Produto::buscarProdutos();
+
+
+?> 
             <header id="topo">
                 <nav>
                     <div class="logo"> <a href="#"><img src="imagens/pratoheader.png"/></a></div> 
@@ -49,27 +71,28 @@
      </div>
    </section>
    <section>
-       
+      
        <ul>
+           <li>
          <?php 
-        $nomeProduto="Pizza Frango Cremoso";
+     
         $nomeRestaurante="Pizzaria do Vizinho";
         $descricao="Pizza saborosa e cremosa<br/>queijo, tomate, frango";
-        $pontuacao=4.5;
+        $pontuacao=4;
         $valor=12;
         $clienteComent=array(array("JOAO","Muito bom!"),array("vitor"," Vale a pena"));
         
         $minhaAvaliacao=3;
 
-         for($i=0;$i<5;$i++){?>
+         foreach($produtos as $produto){?>
             <div class="sect1">
            <div class="item">          
                <div class="imagem"><img src="imagens/pizza.png" alt="promo1"></div> 
                 <div class="info">
                      <div class="titulo">  
-                             <div class="nomeProduto"><?php echo $nomeProduto ?></div>   
-                            <div class="nomeRestaurante"><?php  echo $nomeRestaurante ?></div>    
-                    <div id="descricaoTx" class="descricaoTx"><?php echo $descricao ?>
+                             <div class="nomeProduto"><?php echo $produto->nome_produto; ?></div>   
+                            <div class="nomeRestaurante"><?php $empresa=Empresa::buscarPorId($produto->empresa_idEmpresa);  echo $empresa->nome; ?></div>    
+                    <div id="descricaoTx" class="descricaoTx"><?php echo $produto->descricao ?>
                           
                         <div class="btexp"><button  class="expandirBt" onclick="expandir(this.value)">Ler Mais</button></div>  
                      </div> 
@@ -81,7 +104,7 @@
                            
                              
                              <?php 
-                         
+                         $pontuacao=Cliente_avaliacao_produto::buscarMediaProduto($produto->idProduto);
                              for($j=0;$j<5;$j++){                            
                                  ?>       <?php
                                  $corEstrela="estrela";
@@ -107,7 +130,7 @@
                <div class="compra">
                    <div class="carrinho"><button><img src="imagens/carrinho.png" alt="carrinho"></button></div>
                    <div class="promocao"><img src="imagens/promocao.png" alt="promoção"></div>
-                   <div class="preco"><?php echo 'R$' .number_format($valor,2,',','')  ?></div>
+                   <div class="preco"><?php echo 'R$' .number_format($produto->valor,2,',','')  ?></div>
                 </div> 
                 
            </div>
@@ -142,6 +165,7 @@
                                </label>
                                <label class="myAvaliacao"><button class="estrelaMinhaAvaliacao" onclick="atualizarEstrela(this.value)"><img class ="estrela" src="imagens/estrela.png" alt="*"></button>
                                </label>
+
                                <label class="myAvaliacao"><button class="estrelaMinhaAvaliacao" onclick="atualizarEstrela(this.value)"><img class ="estrela" src="imagens/estrela.png" alt="*"></button>
                                </label>
                                <label class="myAvaliacao"><button class="estrelaMinhaAvaliacao" onclick="atualizarEstrela(this.value)"><img class ="estrela" src="imagens/estrela.png" alt="*"></button>
@@ -170,151 +194,7 @@
         </div>
           <?php }?>
 
-          <!-- <li>
-               <div class="sect1">
-           <div class="item">          
-               <div class="imagem"><img src="imagens/pizza.png" alt="promo1"></div> 
-                <div class="info">
-                     <div class="titulo">  
-                             <div class="nomeProduto">Pizza Frango Cremoso</div>   
-                            <div class="nomeRestaurante">Pizzaria do vizinho</div>    
-                    <div id="descricaoTx" class="descricaoTx">Pizza saborosa e cremosa<br/>queijo, tomate, frango
-                          
-                        <div class="btexp"><button  class="expandirBt" onclick="expandir(this.value)">Ler Mais</button></div>  
-                     </div> 
-                   
-                    <div class="descricao">  
-                        <div class="avaliacao">
-                         <div class="estrelas">
-                             <ul>
-                                 <li><img class ="estrela" src="imagens/estrela.png" alt="*"></li>
-                                 <li><img class ="estrela" src="imagens/estrela.png" alt="*"></li>
-                                 <li><img class ="estrela" src="imagens/estrela.png" alt="*"></li>
-                                 <li><img class ="estrela" src="imagens/estrela.png" alt="*"></li>
-                                 <li><img class ="estrela" src="imagens/estrela.png" alt="*"></li>
-                             </ul>
-                         </div>
-                         <div class="pontuacao">5.0</div>
-                         <div class="avaliacaoBt"><button id="avaliacoesBt" class="avaliacoesBt" onclick="avaliacaoBt(this.value)">Avaliações</button></div>
-                        </div>  
-                       </div> 
-               </div> 
-                        
-                        
-                </div>
-            
-               <div class="compra">
-                   <div class="carrinho"><button><img src="imagens/carrinho.png" alt="carrinho"></button></div>
-                   <div class="promocao"><img src="imagens/promocao.png" alt="promoção"></div>
-                   <div class="preco">R$25,00</div>
-                </div> 
-                
-           </div>
-         
-           <div id="avaliacaoTx" class="avaliacaoTx">
-               <div class="outrosUsuarios">
-                   <div class="minimizarAv"><button class=minimizarBt onclick="minimizarAvaliacao(this.value)">X</button></div>
-                   <div class="listaAvaliacao">Comentários:
-                       <ul>
-                       <li><div class="nomeLista">vitor:</div><div class="textoLista">gostei da pizza, muito boa</div></li>
-                       <li><div class="nomeLista">joao:</div><div class="textoLista">pizza top</div></li>
-                       <li><div class="nomeLista">aline:</div><div class="textoLista">pizza maravilhosa</div></li>
-                      
-                   </ul>
-                </div>
-                   <div class="novoComentario">
-                       <div class="caixaInput"><div>Comente:</div><textarea class="comentario" rows="5" cols="10" maxlength="200" ></textarea> </div>
-                       <div class="enviar"><button class="btEnviar">Enviar</button></div>
-                   </div>
-               </div>
-               <div class="minhaAvaliacao">
-                   <div class="estrelas">
-                    <ul>
-                        <li><button><img class ="estrela" src="imagens/estrela.png" alt="*"></button></li>
-                        <li><button><img class ="estrela" src="imagens/estrela.png" alt="*"></button></li>
-                        <li><button><img class ="estrela" src="imagens/estrela.png" alt="*"></button></li>
-                        <li><button><img class ="estrela" src="imagens/estrela.png" alt="*"></button></li>
-                        <li><button><img class ="estrela" src="imagens/estrela.png" alt="*"></button></li>
-                    </ul>
-                   </div>
-                   <div class="valorPontuacao">5.0</div>
-               </div>
-           </div>         
-        </div>
-    </li>
-   <li>
-        <div class="sect1">
-    <div class="item">          
-        <div class="imagem"><img src="imagens/acai.jpg" alt="promo1"></div> 
-         <div class="info">
-              <div class="titulo">  
-                      <div class="nomeProduto">Açaí</div>   
-                     <div class="nomeRestaurante">Casa da Tia</div>   
-                     <div id="descricaoTx" class="descricaoTx">Melhor açaí da região!<br/>Acompanhamentos:<br/>banana<br/>morango<br/>kiwi<br/>leite condensado
-                        <br/>leite em pó<br/>farinha láctea
-                       
-                 <div class="btexp"><button  class="expandirBt" onclick="expandir(this.value)">Ler Mais</button></div>  
-               
-             </div> 
-             
-             <div class="descricao">  
-                 <div class="avaliacao">
-                  <div class="estrelas">
-                      <ul>
-                          <li><img class ="estrela" src="imagens/estrela.png" alt="*"></li>
-                          <li><img class ="estrela" src="imagens/estrela.png" alt="*"></li>
-                          <li><img class ="estrela" src="imagens/estrela.png" alt="*"></li>
-                          <li><img class ="estrela" src="imagens/estrela.png" alt="*"></li>
-                          <li><img class ="estrela" src="imagens/estrela.png" alt="*"></li>
-                      </ul>
-                  </div>
-                  <div class="pontuacao">5.0</div>
-                  <div class="avaliacaoBt"><button id="avaliacoesBt" class="avaliacoesBt" onclick="avaliacaoBt(this.value)">Avaliações</button></div>
-                 </div>  
-                </div> 
-        </div> 
-                 
-                 
-         </div>
-     
-        <div class="compra">
-            <div class="carrinho"><button><img src="imagens/carrinho.png" alt="carrinho"></button></div>
-            <div class="promocao"><img src="imagens/promocao.png" alt="promoção"></div>
-            <div class="preco">R$8,00</div>
-         </div> 
-         
-    </div>
-  
-    <div id="avaliacaoTx" class="avaliacaoTx">
-        <div class="outrosUsuarios">
-            <div class="minimizarAv"><button class=minimizarBt onclick="minimizarAvaliacao(this.value)">X</button></div>
-            <div class="listaAvaliacao">Comentários:
-                <ul>
-                <li><div class="nomeLista">arthur:</div><div class="textoLista">açaí bom</br></br></div></li>
-                <li><div class="nomeLista">vitor:</div><div class="textoLista">razoável, pouca fruta</div></li>
-                
-            </ul>
-         </div>
-            <div class="novoComentario">
-                <div class="caixaInput"><div>Comente:</div><textarea class="comentario" rows="5" cols="10" maxlength="200" ></textarea> </div>
-                <div class="enviar"><button class="btEnviar">Enviar</button></div>
-            </div>
-        </div>
-        <div class="minhaAvaliacao">
-            <div class="estrelas">
-             <ul>
-                 <li><button><img class ="estrela" src="imagens/estrela.png" alt="*"></button></li>
-                 <li><button><img class ="estrela" src="imagens/estrela.png" alt="*"></button></li>
-                 <li><button><img class ="estrela" src="imagens/estrela.png" alt="*"></button></li>
-                 <li><button><img class ="estrela" src="imagens/estrela.png" alt="*"></button></li>
-                 <li><button><img class ="estrela" src="imagens/estrela.png" alt="*"></button></li>
-             </ul>
-            </div>
-            <div class="valorPontuacao">5.0</div>
-        </div>
-    </div>         
- </div>
-</li>-->
+         </li> 
     </ul>
         
        
