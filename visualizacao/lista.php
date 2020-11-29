b698e696829ab33c3ea2bccf13e232f409cbd6b9
Tiago Menezes <39500750+TiagoMenezesVarmassera@users.noreply.github.com>
2020-11-29 14:06:40 -0400

    Update lista.php

--- visualizacao/lista.php
+++ visualizacao/lista.php
@@ -1,12 +1,9 @@
 <!DOCTYPE html>
 <html>
-
-    <?php
-      if (!isset($_SESSION['usuario'])){
-        header("location: login.php");
-      }
-    ?>
+<?php
     
+    ?>
+
     <head>
         <meta charset="UTF-8">
         <link href='https://fonts.googleapis.com/css?family=Russo One' rel='stylesheet'>
@@ -36,11 +33,20 @@ try{
     echo $th;
     die(1);
 }
-
+// Faz a Busca de todos os Produtos e armazena em $produtos
 $produtos=Produto::buscarProdutos();
-
-
+//Cria a instancia do usuário que está logado no momento através do Post
+$clienteAtual=null;
+if (!isset($_SESSION['usuario'])){
+    header("location: login.php");
+  }else if(isset($_SESSION['usuario'])){
+   
+        $clienteAtual = Cliente::buscar($_POST['email_login']);
+    
+  }
 ?> 
+
+<!--topo da página com menu -->
             <header id="topo">
                 <nav>
                     <div class="logo"> <a href="#"><img src="imagens/pratoheader.png"/></a></div> 
@@ -54,13 +60,14 @@ $produtos=Produto::buscarProdutos();
                 <ul>
                     <li><a href="#suporte">Suporte</a></li>
                     <li><a href="#pedidos">Pedidos</a></li>
-                    <li><a href="inicial.php">sair</a></li>
+                    <li><a href="index.php">sair</a></li>
                   
                 </ul></div>
-            </nav>
-               
+            </nav>            
                   
             </header>
+
+            <!-- Trecho de busca e ordenacao dos produtos-->
   <section class="busca">
       <div>
         <div class="lupa"><img src="imagens/lupa.png" alt="lupa"></div>
@@ -78,37 +85,35 @@ $produtos=Produto::buscarProdutos();
      </div>
    </section>
    <section>
-      
+      <!-- Lista dos Produtos é criado aqui dentro do loop -->
        <ul>
            <li>
          <?php 
-     
-        $nomeRestaurante="Pizzaria do Vizinho";
-        $descricao="Pizza saborosa e cremosa<br/>queijo, tomate, frango";
-        $pontuacao=4;
-        $valor=12;
-        $clienteComent=array(array("JOAO","Muito bom!"),array("vitor"," Vale a pena"));
-        
-        $minhaAvaliacao=3;
-
+            $minhaAvaliacaoPorProduto=0;  // esse valor é colocado dentro da div de class=minhaAvaliacao para ser definido a sequencia do uso do javaScript para atualizar as estrelas
+            
+         //Loop que pega cada produto dentro da lista de produtos
          foreach($produtos as $produto){?>
             <div class="sect1">
-           <div class="item">          
-               <div class="imagem"><img src="imagens/pizza.png" alt="promo1"></div> 
+                <div class="item">          
+                 <div class="imagem"><img src="imagens/pizza.png" alt="promo1"></div> <!-- deve ser colocado o link resgatado do BD para a imagem do produto-->
                 <div class="info">
                      <div class="titulo">  
-                             <div class="nomeProduto"><?php echo $produto->nome_produto; ?></div>   
-                            <div class="nomeRestaurante"><?php $empresa=Empresa::buscarPorId($produto->empresa_idEmpresa);  echo $empresa->nome; ?></div>    
+                          <!--Resgata o nome do produto-->
+                             <div class="nomeProduto"><?php echo $produto->nome_produto; ?></div>  
+                             <!-- Cria uma insância da empresa dona desse produto pelo id da empresa registrado na tabela de produto e emite seu nome-->
+                            <div class="nomeRestaurante"><?php $empresa=Empresa::buscarPorId($produto->empresa_idEmpresa);  echo $empresa->nome; ?></div> 
+                            <!-- Apresente a descrição do produto-->   
                     <div id="descricaoTx" class="descricaoTx"><?php echo $produto->descricao ?>
-                          
+                          <!--Botão para expandir a descricao do produto -->
                         <div class="btexp"><button  class="expandirBt" onclick="expandir(this.value)">Ler Mais</button></div>  
                      </div> 
                    
                     <div class="descricao">  
+                        <!-- bloco da media de avalicao do produto -->
                         <div class="avaliacao">
                          <div class="estrelas">
                              <ul> 
-                             <?php 
+                             <?php //Busca a media da avaliacao do produto e configura as estrelas
                                 $pontuacao=Cliente_avaliacao_produto::buscarMediaProduto($produto->idProduto);
                                     for($j=0;$j<5;$j++){                            
                                         ?>       <?php
@@ -123,77 +128,82 @@ $produtos=Produto::buscarProdutos();
                              
                              </ul>
                          </div>
+                         <!--imprime o valor arredondado da media da avaliacao do produto-->
                          <div class="pontuacao"><?php echo number_format(floor($pontuacao),1,'.','')  ?></div>
                          <div class="avaliacaoBt"><button id="avaliacoesBt" class="avaliacoesBt" onclick="avaliacaoBt(this.value)">Avaliações</button></div>
                         </div>  
-                       </div> 
+                    </div> 
                </div> 
                         
                         
                 </div>
             
                <div class="compra">
-                   <div class="carrinho"><button><img src="imagens/carrinho.png" alt="carrinho"></button></div>
-                   <div class="promocao"><img src="imagens/promocao.png" alt="promoção"></div>
-                   <div class="preco"><?php echo 'R$' .number_format($produto->valor,2,',','')  ?></div>
+                   <div class="carrinho"><button><img src="imagens/carrinho.png" alt="carrinho"></button></div> <!--deve implementar a funcao de add ao carrinho-->
+                   <div class="promocao"><img src="imagens/promocao.png" alt="promoção"></div> <!--deve aparecer somente se tiver desconto-->
+                   <div class="preco"><?php echo 'R$' .number_format($produto->valor-$produto->valorDesconto,2,',','')  ?></div> <!--Valor do produto com Desconto-->
                 </div> 
                 
            </div>
          
            <div id="avaliacaoTx" class="avaliacaoTx">
+               <!--Avaliacao em comentário de clientes salvo no banco de dados -->
                <div class="outrosUsuarios">
                    <div class="minimizarAv"><button class=minimizarBt onclick="minimizarAvaliacao(this.value)">X</button></div>
                    <div class="listaAvaliacao">Comentários:
-                       <ul>
+                       <ul> <!--lista de comentários-->
                        <?php 
-                         $comentarios=Comentario::buscarTodos($produto->idProduto);
+                       // Carrega a lista de todos os comentarios para este produto
+                       $comentarios=Comentario::buscarTodos($produto->idProduto);
+                       //manipula cada comentário ate imprimir todos
                                foreach($comentarios as $comentario)   {               
-                                $cliente=Cliente::buscarPorId($comentario->cliente_idCliente);
-                             ?> 
+                                   //carrega o nome do cliente que fez este comentário
+                                $cliente= Cliente::buscarPorId($comentario->cliente_idCliente);?> 
+                                 <!--imprime o nome do usuario e o comentário feito-->
                                <li><div class="nomeLista"><?php echo $cliente->nome; ?></div><div class="textoLista"><?php echo $comentario->texto; ?></div></li>
-                                 <?php
-                               }                                
-                          ?>
-                      
-                      
+                         <?php }  ?>                                            
                    </ul>
                 </div>
+                <!--Campo para preenchimento de novo comentário-->
+                <!--Deve ser implementado um salvamento para o banco de dados-->
                    <div class="novoComentario">
                        <div class="caixaInput"><div>Comente:</div><textarea class="comentario" rows="5" cols="10" maxlength="200" ></textarea> </div>
                        <div class="enviar"><button class="btEnviar">Enviar</button></div>
                    </div>
                </div>
-               <div class="minhaAvaliacao">
-                   <div class="estrelas"><form><label class="myAvaliacao"><button class="estrelaMinhaAvaliacao" onclick="atualizarEstrela(this.value)"><img class ="estrela" src="imagens/estrela.png" alt="*"></button>
-                               </label>
-                               <label class="myAvaliacao"><button class="estrelaMinhaAvaliacao" onclick="atualizarEstrela(this.value)"><img class ="estrela" src="imagens/estrela.png" alt="*"></button>
-                               </label>
-                               <label class="myAvaliacao"><button class="estrelaMinhaAvaliacao" onclick="atualizarEstrela(this.value)"><img class ="estrela" src="imagens/estrela.png" alt="*"></button>
-                               </label>
-
-                               <label class="myAvaliacao"><button class="estrelaMinhaAvaliacao" onclick="atualizarEstrela(this.value)"><img class ="estrela" src="imagens/estrela.png" alt="*"></button>
-                               </label>
-                               <label class="myAvaliacao"><button class="estrelaMinhaAvaliacao" onclick="atualizarEstrela(this.value)"><img class ="estrela" src="imagens/estrela.png" alt="*"></button>
-                               </label></form>
-                 <!--   <ul class="myAvaliacao" value="--><?php /*echo $minhaAvaliacao ?>">
-                    <?php 
-                        
-                         for($j=0;$j<5;$j++){                            
-                             ?>       <?php
-                             $corEstrela="estrela";
-                              if($j>=floor($minhaAvaliacao)){ ?> 
-                               <li><button class="estrelaMinhaAvaliacao" onclick="atualizarEstrela(this.value)" value="<?php echo $val=$j+1;?>" ><img class ="estrela estrelaApagada" src="imagens/estrela.png" alt="*"></button></li>
-                               
-                              <?php  }else {?> 
-                                <li><button class="estrelaMinhaAvaliacao" onclick="atualizarEstrela(this.value)" value="<?php echo $val=$j+1;?>"><img class ="estrela" src="imagens/estrela.png" alt="*"></button></li>
-                               
-                                 <?php } 
-                                                               
-                          } ?>
-                       
-                    </ul>*/?><!--apagar -->
-                   </div>
-                   <div class="valorPontuacao"><?php echo number_format(floor($minhaAvaliacao),1,'.','') ?></div>
+               <!--Parte onde fica a avaliacao do usuario logado. O value dessa div recebe o valor que é incrementado para cada produto afim de ser manipulado no javaScript-->
+               <div class="minhaAvaliacao" value=<?php  echo $minhaAvaliacaoPorProduto; ?>>
+                        <?php 
+                        //carrega a pontuacao do clienteAtual 
+                         $pontuacao=Cliente_avaliacao_produto::buscar($clienteAtual->idCliente, $produto->idProduto);
+                         //se nao hover avaliacao deste usuario, a pontuacao fica como zero mas sem registrar ainda no banco de dados. Se houver, utiliza a pontuacao para visualizacao
+                        if(is_null($pontuacao)){
+                            $pontuacao=0;
+                        }else{
+                            $pontuacao=$pontuacao->valorAvaliacao;
+                        }
+                        ?>
+                        <!--div das estrelas dadas pelo cliente logado-->
+                        <div class="estrelas">
+                            <ul>
+                            <!-- for utilizado para garregar as 5 estrelas confore a avaliacao do usuario-->
+                                <?php for($j=0;$j<5;$j++){  ?> <?php
+                                    $corEstrela="estrela";
+                                    if($j>=floor($pontuacao)){ ?> 
+                                    <li><button class="estrelaMinhaAvaliacao" value="<?php echo $j+1;?>" onclick="atualizarEstrela(this.value, <?php echo $produto->idProduto?>,<?php echo $minhaAvaliacaoPorProduto ?>)"><img class ="estrela estrelaApagada" src="imagens/estrela.png" alt="*"></button></li>
+                                    <?php  }else {?> 
+                                        <li><button class="estrelaMinhaAvaliacao" value="<?php echo $j+1;?>" onclick="atualizarEstrela(this.value, <?php echo $produto->idProduto?>, <?php echo $minhaAvaliacaoPorProduto ?>)"><img class ="estrela" src="imagens/estrela.png" alt="*"></button></li>
+                                    
+                                        <?php } 
+                                                                    
+                                } 
+                                // valor é incrementado para dar a posicao de cada avaliacao em cada produto,  para ser utilizdo na chamada da funcao do javaScript 
+                                $minhaAvaliacaoPorProduto=$minhaAvaliacaoPorProduto+1;?>                       
+                          </ul>                          
+            
+                        </div>
+                   <!--imprime o valor da pontuacao do cliente logado para este produto -->
+                     <div class="valorPontuacao"><?php echo $pontuacao; ?></div>
                </div>
            </div>         
         </div>
